273b0485063f847ae7483d09e62577d20c14a254
Stepan Kutaj <stepan.kutaj@tldr-it.com>
2025-04-17 13:46:44 +0200

    Final - v1.0.0 - SKU

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -5,6 +5,7 @@
 #include "../include/thread_pool.hpp"
 #include <atomic>
 #include <iostream>
+#include <mutex>
 
 Scanner::Scanner(size_t threads, std::string mode, int port)
     : threadCount(threads), mode(std::move(mode)), port(port) {
@@ -14,24 +15,40 @@ void Scanner::run(const std::string &cidr) const {
     auto [startIp, endIp] = Utils::parseCIDR(cidr);
     ThreadPool pool(threadCount);
     std::atomic<int> counter = 0;
+    std::mutex printMutex;
     const uint32_t total = endIp - startIp + 1;
 
     for (uint32_t ip = startIp; ip <= endIp; ++ip) {
-        pool.enqueue([ip, &counter, total, this] {
+        pool.enqueue([ip, &counter, total, &printMutex, this] {
             const std::string ipStr = Utils::uintToIp(ip);
 
+            bool isAlive = false;
+
             if (mode == "icmp") {
-                if (Icmp::ping(ipStr, counter, total)) {
-                    std::cout << "\n" << ipStr << "\n";
-                }
+                isAlive = Icmp::ping(ipStr);
             } else if (mode == "tcp") {
-                Tcp::ping(ipStr, port);
+                isAlive = Tcp::ping(ipStr, port);
             } else if (mode == "fallback") {
-                if (!Icmp::ping(ipStr, counter, total)) {
-                    Tcp::ping(ipStr, port);
-                } else {
-                    std::cout << "\n" << ipStr << "\n";
+                isAlive = Icmp::ping(ipStr);
+                if (!isAlive) {
+                    isAlive = Tcp::ping(ipStr, port);
                 }
+            }
+
+            int done = ++counter;
+
+            // LOCK: tisk pouze 1 threadem najednou
+            std::lock_guard<std::mutex> lock(printMutex);
+
+            std::cerr << "\r[";
+            const int width = 30;
+            int filled = (done * width) / total;
+            for (int i = 0; i < width; ++i)
+                std::cerr << (i < filled ? '#' : '.');
+            std::cerr << "] " << done << "/" << total << std::flush;
+
+            if (isAlive) {
+                std::cout << ipStr << "\n";
             }
         });
     }
